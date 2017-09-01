1858954675220d4dd456a8b93ea5c94b849d5e78
Vivian Huang <vivianhuang0130@gmail.com>
2017-09-01 15:07:25 -0700

    fix AWS error on Heroku test #5

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -11,7 +11,7 @@ class Picture < ApplicationRecord
 
 def s3_credentials
   {
-    bucket: ENV['AWS_BUCKET'],
+    bucket: ENV['AWS_BUCKET_NAME'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
   }
