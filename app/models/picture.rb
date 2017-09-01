59cd68f4bf06ceb94b9106e26524a69789a029fb
Vivian Huang <vivianhuang0130@gmail.com>
2017-09-01 14:37:09 -0700

    fix AWS error on Heroku

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,14 +6,14 @@ class Picture < ApplicationRecord
                     storage: :s3,
                     url: ":s3_domain_url",
                     path: "/:class/:id_partition/:style/:filename",
-                    s3_region: ENV["S3_REGION"],
+                    s3_region: ENV["AWS_REGION"],
                     s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
 
 def s3_credentials
   {
     bucket: ENV["S3_BUCKET_NAME"],
-    access_key_id: ENV["S3_ACCESS_KEY_ID"],
-    secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
 end
 
