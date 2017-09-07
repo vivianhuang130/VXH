a4bdc635673d5cd590637beaa185619d0ae6e137
Vivian Huang <vivianhuang0130@gmail.com>
2017-09-07 14:48:13 -0700

    local photo uploading fixed, still not able to deploy aws onto heroku

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,18 +2,18 @@ class Picture < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_attached_file :url,
-                    styles: {medium: '500x500#', thumb: '100x100#'},
+                    styles: {medium: "500x500#", thumb: "100x100#"},
                     storage: :s3,
-                    url: ':s3_domain_url',
-                    path: '/:class/:id_partition/:style/:filename',
-                    s3_region: 'us-west-2',
+                    url: ":s3_domain_url",
+                    path: "/:class/:id_partition/:style/:filename",
+                    s3_region: ENV["S3_REGION"],
                     s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
 
 def s3_credentials
   {
-    bucket: ENV['AWS_BUCKET_NAME'],
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    bucket: ENV["S3_BUCKET_NAME"],
+    access_key_id: ENV["S3_ACCESS_KEY_ID"],
+    secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
   }
 end
 
